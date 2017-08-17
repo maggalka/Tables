646cea1736a299209851ac15fc43b5da501fa0a0
Marina <maggalka@mail.ru>
2017-08-17 10:54:41 +0300

    Service interfaces implemented.

--- src/main/java/uz/marina/tables/service/ElementServiceImpl.java
+++ src/main/java/uz/marina/tables/service/ElementServiceImpl.java
@@ -1,7 +1,46 @@
 package uz.marina.tables.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import uz.marina.tables.dao.ElementDao;
+import uz.marina.tables.model.Element;
+
+import java.util.List;
+
 /**
  * Created by Marina on 16.08.2017.
  */
-public class ElementServiceImpl {
+@Transactional(readOnly = true)
+public class ElementServiceImpl implements ElementService {
+
+    @Autowired
+    private ElementDao elementDao;
+
+    @Override
+    @Transactional
+    public void addElement(Element element) {
+        elementDao.addElement(element);
+    }
+
+    @Override
+    @Transactional
+    public void updateElement(Element element) {
+        elementDao.updateElement(element);
+    }
+
+    @Override
+    public List<Element> listElementsByColumnValue(Class<? extends Element> elementClass, String columnName, int value) {
+        return elementDao.listElementsByColumnValue(elementClass,columnName,value);
+    }
+
+    @Override
+    @Transactional
+    public void removeElementsByColumnValue(Class<? extends Element> elementClass, String columnName, int value) {
+        elementDao.removeElementsByColumnValue(elementClass,columnName,value);
+    }
+
+    @Override
+    public List<Element> listElements(String className, Long page) {
+        return elementDao.listElements(className,page);
+    }
 }
--- src/main/java/uz/marina/tables/service/ElementWithIdServiceImpl.java
+++ src/main/java/uz/marina/tables/service/ElementWithIdServiceImpl.java
@@ -1,7 +1,28 @@
 package uz.marina.tables.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import uz.marina.tables.dao.ElementWithIdDao;
+import uz.marina.tables.model.Element;
+
 /**
  * Created by Marina on 16.08.2017.
  */
-public class ElementWithIdServiceImpl {
+
+@Transactional(readOnly = true)
+public class ElementWithIdServiceImpl implements ElementWithIdService{
+
+    @Autowired
+    private ElementWithIdDao elementWithIdDao;
+
+    @Override
+    public Element getElementById(Class<? extends Element> elementClass, int id) {
+        return elementWithIdDao.getElementById(elementClass,id);
+    }
+
+    @Transactional
+    @Override
+    public void removeElementById(Class<? extends Element> elementClass, int id) {
+        elementWithIdDao.removeElementById(elementClass,id);
+    }
 }
